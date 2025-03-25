3c5375153c7702f79eadbb80e52d9ada9a5c7883
amr sobhy <amr.elsayed.sobhy@gmail.com>
2025-03-25 15:07:33 +0200

    Initial Commit

---
+++ MazadyUI/MazadyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazadyUIApp.swift
+//  MazadyUI
+//
+//  Created by mio on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MazadyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
